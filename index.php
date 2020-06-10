5a03a69a2cfae997ee3fe2514c3746c6e4a142b9
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-10 15:53:17 +0200

    debut du TP

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tp upload</title>
+</head>
+
+<body>
+    <h1>tp upload</h1>
+
+
+
+</body>
+
+</html>
