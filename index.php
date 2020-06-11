12892bbb150d3ac2c8e2c433cdb0f0d618295a28
yvesmaried <yvesmarie.drouard@gmail.com>
2020-06-11 09:58:18 +0200

    phpadd

--- index.php
+++ index.php
@@ -1,3 +1,100 @@
+<?php
+ 
+// Constantes
+define('TARGET', '/files/');    // Repertoire cible
+define('MAX_SIZE', 100000);    // Taille max en octets du fichier
+define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
+define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
+ 
+// Tableaux de donnees
+$tabExt = array('jpg','gif','png','jpeg');    // Extensions autorisees
+$infosImg = array();
+ 
+// Variables
+$extension = '';
+$message = '';
+$nomImage = '';
+ 
+/************************************************************
+ * Creation du repertoire cible si inexistant
+ *************************************************************/
+if( !is_dir(TARGET) ) {
+  if( !mkdir(TARGET, 0755) ) {
+    exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
+  }
+}
+ 
+/************************************************************
+ * Script d'upload
+ *************************************************************/
+if(!empty($_POST))
+{
+  // On verifie si le champ est rempli
+  if( !empty($_FILES['fichier']['name']) )
+  {
+    // Recuperation de l'extension du fichier
+    $extension  = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
+ 
+    // On verifie l'extension du fichier
+    if(in_array(strtolower($extension),$tabExt))
+    {
+      // On recupere les dimensions du fichier
+      $infosImg = getimagesize($_FILES['fichier']['tmp_name']);
+ 
+      // On verifie le type de l'image
+      if($infosImg[2] >= 1 && $infosImg[2] <= 14)
+      {
+        // On verifie les dimensions et taille de l'image
+        if(($infosImg[0] <= WIDTH_MAX) && ($infosImg[1] <= HEIGHT_MAX) && (filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE))
+        {
+          // Parcours du tableau d'erreurs
+          if(isset($_FILES['fichier']['error']) 
+            && UPLOAD_ERR_OK === $_FILES['fichier']['error'])
+          {
+            // On renomme le fichier
+            $nomImage = md5(uniqid()) .'.'. $extension;
+ 
+            // Si c'est OK, on teste l'upload
+            if(move_uploaded_file($_FILES['fichier']['tmp_name'], TARGET.$nomImage))
+            {
+              $message = 'Upload réussi !';
+            }
+            else
+            {
+              // Sinon on affiche une erreur systeme
+              $message = 'Problème lors de l\'upload !';
+            }
+          }
+          else
+          {
+            $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
+          }
+        }
+        else
+        {
+          // Sinon erreur sur les dimensions et taille de l'image
+          $message = 'Erreur dans les dimensions de l\'image !';
+        }
+      }
+      else
+      {
+        // Sinon erreur sur le type de l'image
+        $message = 'Le fichier à uploader n\'est pas une image !';
+      }
+    }
+    else
+    {
+      // Sinon on affiche une erreur pour l'extension
+      $message = 'L\'extension du fichier est incorrecte !';
+    }
+  }
+  else
+  {
+    // Sinon on affiche une erreur pour le champ vide
+    $message = 'Veuillez remplir le formulaire svp !';
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -7,56 +104,76 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="assets\style.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
     <link href="img\icon.png" rel="icon">
 
     <title>tp upload</title>
 </head>
 
 <body>
+
     <!------------------------------------------------- HEADER ------------------------------->
-<header>
-    <div id="img-accueil" class="container-fluid">
-        <div class="row justify-content-lg-start justify-content-center text-center">
-            <div class="col-12 justify-content-center text-center">
-                <h1 class="typo-specialelite" id="titreHeader">Bovis-Shark</h1>
+    <header>
+        <div id="img-accueil" class="container-fluid">
+            <div class="row justify-content-lg-start justify-content-center text-center">
+                <div class="col-12 justify-content-center text-center">
+                    <h1 class="typo-specialelite" id="titreHeader">Bovis-Shark</h1>
+                </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
     <!------------------------------------------------- MAIN ------------------------------->
-<main id="id-main">
-    <div class="container">
-        <div class="row">
-            <div class="row col-12 my-5 shadow" id="id-workarea">
-                <!------------------------------------------------- COL GAUCHE ------------------------------->
-                <div class="col-6 border my-5">
-                    <h1>zone 1</h1>
+    <main id="id-main">
+        <div class="container">
+            <div class="row">
+                <div class="row col-12 my-5 shadow" id="id-workarea">
+                    <!------------------------------------------------- COL GAUCHE ------------------------------->
+                    <div class="col-6 border my-5">
+                        <form enctype="multipart/form-data"
+                            action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                            <fieldset>
+                                <legend>Formulaire</legend>
+                                <p>
+                                    <label for="fichier_a_uploader" title="Recherchez le fichier à uploader !">Envoyer
+                                        le fichier :</label>
+                                    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MAX_SIZE; ?>" />
+                                    <input name="fichier" type="file" id="fichier_a_uploader" />
+                                    <input type="submit" name="submit" value="Uploader" />
+                                </p>
+                            </fieldset>
+                        </form>
+                        <?php
+                            if(!empty($message)){ 
+                                echo '<p>';
+                                echo "\t\t<strong>", htmlspecialchars($message) ,"</strong>";
+                                echo "\t</p>";
+                            }
+                        ?>
+                    </div>
+
+                    <!------------------------------------------------- COL DROITE ------------------------------->
+                    <div class="col-6 border my-5">
+                        <h1>zone 2</h1>
+
+                    </div>
 
                 </div>
-                <!------------------------------------------------- COL DROITE ------------------------------->
-                <div class="col-6 border my-5">
-                    <h1>zone 2</h1>
-
-                </div>
-
             </div>
         </div>
-    </div>
-</main>
+    </main>
     <!------------------------------------------------- FOOTER ------------------------------->
-<footer>
-    <div class="container-fluid">
-        <div class="row justify-content-center text-center">
-            <div class="col-12 foot1">
+    <footer>
+        <div class="container-fluid">
+            <div class="row justify-content-center text-center">
+                <div class="col-12 foot1">
                     <span>test123test</span>
-            </div>
-            <div class="col-12 foot2">
+                </div>
+                <div class="col-12 foot2">
                     <span>@copiright bla bla</span>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
 
 
